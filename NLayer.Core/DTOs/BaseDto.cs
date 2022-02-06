e37a8ee6512730d62d6d656d149c8694a7a16367
Hakan GUL <hakanngul@icloud.com>
2022-02-06 20:33:07 +0300

    API Layer Complated

---
+++ NLayer.Core/DTOs/BaseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NLayer.Core.DTOs
+{
+    public abstract class BaseDto
+    {
+        public int Id { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
+    }
+}
---
+++ NLayer.Service/Validations/ProductDtoValidation.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using NLayer.Core.DTOs;
+
+namespace NLayer.Service.Validations
+{
+    public class ProductDtoValidation : AbstractValidator<ProductDto>
+    {
+        public ProductDtoValidation()
+        {
+            RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is required").NotEmpty()
+                .WithMessage("{PropertyName} is required");
+            RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
+            RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
+            RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
+
+        }
+    }
+}
